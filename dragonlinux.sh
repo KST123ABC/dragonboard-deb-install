1ea06d14aeafde4b58c12e8099e1eab0848b5ab3
kst123abc <kylesuero@gmail.com>
2017-09-14 15:52:51 -0400

    only downloads if update to current hardcoded image. only unzips if new files

--- dragonlinux.sh
+++ dragonlinux.sh
@@ -2,11 +2,11 @@
 # Download dragonboard 410c debian image, wipe given sd card, install image
 # AUTHOR: Kyle Suero (KST123ABC)
 
-#curl -LOk http://builds.96boards.org/releases/dragonboard410c/linaro/debian/latest/dragonboard410c_sdcard_developer_debian-260.zip
+curl -LOkz dragonboard410c_sdcard_developer_debian-260.zip  http://builds.96boards.org/releases/dragonboard410c/linaro/debian/latest/dragonboard410c_sdcard_developer_debian-260.zip
 echo "Type the path of the micro sd card you want to install the OS to (run lsblk to find sd card. ex: '/dev/sdb'), followed by [ENTER]: "
 
 read -r card
 
-unzip dragonboard410c_sdcard_developer_debian-260.zip
+unzip -f dragonboard410c_sdcard_developer_debian-260.zip
 sudo dd if= db410c_sd_install_*.img of=$card bs=4M oflag=sync status=noxfer
 echo "Set S6 switch on DragonBoard 410c to 0-1-0-0. SD Boot Switch should be set to ON. See 1.1 Board Overview page 7 for more assistance with S6 http://linaro.co/96b-hwm-db"
